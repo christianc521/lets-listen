b40070bdd85caf1fbf07e159fc3af8b92f06979c
Christian Cruz <seseko98@gmail.com>
2021-10-22 20:31:14 -0500

    Added files

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const spotifyWebApi = require('spotify-web-api-node');
+const app = express();
+
+app.post('/login', (req, res) => {
+    const code = req.body.code
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000',
+        clientId: 'eab57081c0654ebfb1c55a4e599089d6',
+        clientSecret: 'dce25173f174441a91ba7d3356b27aba'
+    })
+
+    spotifyApi.authorizationCodeGrant(code).then(data => {
+        res.json({ 
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in
+        })
+    }) 
+    .catch(() => {
+        res.sendStatus(400)
+    })
+})
